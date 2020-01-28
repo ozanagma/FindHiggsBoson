b4ecd15bb2c44606a19790fb08d4f4d7db53d67e
ozan <ozan@OZAN-WIN7>
2020-01-28 23:23:39 +0300

    data prepocessing part added

---
+++ FindHiggsBoson/DataHandler.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+def standardize_data(data):
+    mean_data = np.mean(data, axis = 0)
+    data = data - mean_data
+    std_dev_data_x = np.std(data, axis=0)
+    data[:, std_dev_data_x > 0] = data[:, std_dev_data_x > 0] / std_dev_data_x[std_dev_data_x > 0]
+
+    return data
+
+
+def nan2median(data):
+
+    data[data==-999] = np.nan # changing value -999 with NaN
+    data = data[:, ~np.all(np.isnan(data), axis = 0)] # deleting columns with NaN 
+    data = data[:, ~np.all(data[1:] == data[:1], axis = 0)] # deleting same columns
+    column_median = np.nanmedian(data, axis = 0)  # taking column median  # TODO: discuss median or mean? to:group date:28.01.20
+    nan_indicies = np.where(np.isnan(data)) # extracting indicies of columns with NaN
+    data[nan_indicies] = np.take(column_median, nan_indicies[1]) # changing rows with column median
+
+    return data
+
+def log_transform(data):
+   
+    data = np.log(1 + data) # TODO: is all columns need log transformation to:group date:28.01.20
+
+    return data
+
+
+def load_csv_data(data_path):
+
+    labels = np.genfromtxt(data_path, delimiter=",", skip_header=1, dtype=str, usecols=1)
+    features = np.genfromtxt(data_path, delimiter=",", skip_header=1)
+    data = features[:, 2:]
+
+    labels_bin = np.ones(len(labels))
+    labels_bin[np.where(labels=='b')] = 0
+
+    return labels, data
--- FindHiggsBoson/FindHiggsBoson.py
+++ FindHiggsBoson/FindHiggsBoson.py
@@ -1,10 +1,22 @@
 
-import pandas
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plot
 
-train_data = pandas.read_csv(r'data/training.csv')  #reading train data
+from DataHandler import *
 
-test_data = pandas.read_csv(r'data/test.csv') # reading test data
+train_labels, train_data, traind_event_id_ = load_csv_data("data/training.csv")  #reading train data
+train_data = nan2median(train_data) # changing NaN values with column median
+train_data = log_transform(train_data); # Implementing log transformation to data be less skewed
+train_data = standardize_data(train_data) # Standardization data because variables have diffrent units
 
-print(train_data)
+# For debug only
+plot.hist(train_data[:, 2], bins = 'auto')
+plot.show()
+
+
+
+#test_data = pd.read_csv(r'data/test.csv') # reading test data
+
 
 
