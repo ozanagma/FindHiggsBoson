c6eb8f1b7ae1e79fc59889b783f8854e85b8f7bd
ozan <ozan@OZAN-WIN7>
2020-01-28 21:00:25 +0300

    train andtest data is read

--- FindHiggsBoson/FindHiggsBoson.py
+++ FindHiggsBoson/FindHiggsBoson.py
@@ -1,1 +1,10 @@
 
+import pandas
+
+train_data = pandas.read_csv(r'data/training.csv')  #reading train data
+
+test_data = pandas.read_csv(r'data/test.csv') # reading test data
+
+print(train_data)
+
+
