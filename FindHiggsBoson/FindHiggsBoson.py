cc976803fcc39fd30c94771851d346f63ef37861
Ozan Ağma <49492356+ozanagma@users.noreply.github.com>
2020-03-23 16:40:58 +0300

    Neural Network updated

--- FindHiggsBoson/FindHiggsBoson.py
+++ FindHiggsBoson/FindHiggsBoson.py
@@ -3,7 +3,6 @@ import re
 import time
 import pandas as pd
 import numpy as np
-from random import seed
 
 from DataHandler import *
 from GradientDescent import *
@@ -31,9 +30,19 @@ train_features, validation_features, test_features  = SplitData(features)
 train_labels,   validation_labels,   test_labels    = SplitData(labels)
 
 
-predictions = Train(np.append(test_features.to_numpy(), test_labels.to_numpy(), 1) , 0.5, 20, 2)
 
-print((predictions == test_labels).mean())
+dataset = np.append(test_features.to_numpy(), test_labels.to_numpy(), 1)
+
+
+
+
+#network = InitializeNetwork(30, 80)
+#Train(network, dataset , 0.5, 20)
+#predictions = Predict(network, dataset)
+#print((predictions == test_labels.to_numpy()).mean())
+
+
+
 
 # Get the parameters of the algorithm.
 
--- FindHiggsBoson/NeuralNetwork.py
+++ FindHiggsBoson/NeuralNetwork.py
@@ -1,40 +1,44 @@
 from random import random
+from random import seed
+
 from math import exp
 import numpy as np
 
 def InitializeNetwork(n_inputs, n_hidden):
 	network = list()
-	hidden_layer = [{'weights':[random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
+	seed(1)
+	hidden_layer = [{'weights':[((random() -0.5) * 4) for i in range(n_inputs + 1)]} for i in range(n_hidden)]
 	network.append(hidden_layer)
-	output_layer = [{'weights':[random() for i in range(n_hidden + 1)]} for i in range(2)]
+	output_layer = [{'weights':[((random() -0.5) * 4) for i in range(n_hidden + 1)]} for i in range(2)]
 	network.append(output_layer)
 
 	return network
 
 def ActivateNeuron(weights, inputs):
-	activation = weights[-1]
+	bias = weights[-1] 
+	activation = bias
 	for i in range(len(weights)-1):
 		activation += weights[i] * inputs[i]
 
 	return activation
 
-def Sigmoid(neuron_activation):
-	return 1.0 / (1.0 + exp(-neuron_activation))
+def Sigmoid(val):
+	return 1.0 / (1.0 + exp(-val))
 
 
-def ForwardPropagate(network, row):
-	inputs = row
+def ForwardPropagate(network, input):
+	layer_input = input
 	for layer in network:
-		new_inputs = []
+		layer_output = []
 		for neuron in layer:
-			neuron['output'] = Sigmoid(ActivateNeuron(neuron['weights'], inputs))
-			new_inputs.append(neuron['output'])
-		inputs = new_inputs
-	return inputs
+			neuron['output'] = Sigmoid(ActivateNeuron(neuron['weights'], layer_input))
+			layer_output.append(neuron['output'])
+		layer_input = layer_output #output of previous layer will be the input of next layer
+	return layer_input
 
 
-def TakeDerivative(output):
-	return output * (1.0 - output)
+def TakeDerivative(val):
+	return val * (1.0 - val)
 
 def BackwardPropagate(network, expected):
 	for i in reversed(range(len(network))):
@@ -66,25 +70,29 @@ def UpdateWeights(network, row, l_rate):
 				neuron['weights'][j] += l_rate * neuron['delta'] * inputs[j]
 			neuron['weights'][-1] += l_rate * neuron['delta']
 
-def Train(data, l_rate, n_epoch, n_hidden):
-	network = InitializeNetwork(data.shape[1] - 1, n_hidden)
+def Train(network, data, l_rate, n_epoch):
+	
 	for epoch in range(n_epoch):
 		sum_error = 0	
 		for row in data:
-			i = 0
-			outputs = ForwardPropagate(network, row)
+			output = ForwardPropagate(network, row)
 			expected = [0 for i in range(2)]
 			expected[row[-1]] = 1
-			sum_error += sum([(expected[i]-outputs[i])**2 for i in range(len(expected))])
+			sum_error += sum([(expected[i]-output[i])**2 for i in range(len(expected))])
 			BackwardPropagate(network, expected)
 			UpdateWeights(network, row, l_rate)
-			if(epoch == n_epoch - 1):
-				predictions = np.zeros((data.shape[0], 1))
-				pred_data = 1 if outputs[1] >= outputs[0] else 0
-				predictions[i] = pred_data
-				++i
 		print('>epoch=%d, error=%.3f' % (epoch, sum_error / len(data) ))
 
 
-
+def Predict(network, data):
+	predictions = list()
+	for input in data:
+		output = ForwardPropagate(network, input)
+		predictions.append(output.index(max(output)))
+	
 	return predictions
+
+
+def predict(network, row):
+	outputs = ForwardPropagate(network, row)
+	return outputs.index(max(outputs))
